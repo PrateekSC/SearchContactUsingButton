60a69f0750b5f872f08f6591a3db4d5ff203b2d5
PrateekSC <59971293+PrateekSC@users.noreply.github.com>
2020-06-30 18:27:00 +0530

    Add files via upload

--- helloWorld.js
+++ helloWorld.js
@@ -1,49 +1,16 @@
 import { LightningElement, wire, track } from 'lwc';
 
-import getContactList from '@salesforce/apex/ContactController.getContactList';
 import findContacts from '@salesforce/apex/ContactController.findContacts';
 
-const DELAY = 300;
-
-
 export default class HelloWorld extends LightningElement {
-  @track contactsListByLoad;
+  @track contactsList;
   @track error;
-
-  greeting = 'Hello World!';
 
   connectedCallback() {
   }
 
   disconnectedCallback() {
   }
-
-  greetingChangeHandler(event) {
-    this.greeting = event.target.value;
-  }
-
-  handleLoadContactClick() {
-    getContactList()
-    .then(result => {
-        this.contactsListByLoad = result;
-    })
-    .catch(error => {
-        this.error = error;
-    });
-  }
-  
-  searchKey = '';
-  @wire(findContacts, { searchKey: '$searchKey' })
-  autoSearchedContacts;
-
-  handleAutoloadInputChange(event) {
-      window.clearTimeout(this.delayTimeout);
-      const searchKey = event.target.value;
-      this.delayTimeout = setTimeout(() => {
-          this.searchKey = searchKey;
-      }, DELAY);
-  }
-
 
   searchAccount= '';
   handleSearchInputChange(event) {
